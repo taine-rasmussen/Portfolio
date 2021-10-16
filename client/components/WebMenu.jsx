861f212907beb808002f821ed1956edd795fee74
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-16 16:59:00 +1300

    set up webmenu component

--- client/components/WebMenu.jsx
+++ client/components/WebMenu.jsx
@@ -3,8 +3,22 @@ import React from 'react';
 const WebMenu = () => {
 
 	return(
-		<div>
-			web menu
+		<div className="webmenu-container">
+			<div className="webmenu-option">
+				<a href="#project">
+					<i className="fi fi-rr-edit-alt"></i> Projects
+				</a>
+			</div>
+			<div className="webmenu-option">
+				<a href="#skills">
+					<i className="fi fi-rr-computer"></i> Skills
+				</a>
+			</div>
+			<div className="webmenu-option">
+				<a href="#contact">
+					<i className="fi fi-rr-address-book"></i> Contact
+				</a>
+			</div>
 		</div>
 	)
 }
