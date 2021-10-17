779f15eeef0602bd61ee44345f271443deb85506
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-17 19:46:31 +1300

    removed separator from footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../styles/Footer.scss'
 
-// Components
-import Separator from './Separator'
 
 const Footer = () => {  
   return(
       <div>
-        <Separator />
-        Footer
+
+        Footer\
       </div>
   )
 }
