7d53fd5e7ca01498b23275c813f4c0a83b707fa8
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-17 17:17:37 +1300

    finsihed mark up for skills component

--- client/components/Skills.jsx
+++ client/components/Skills.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
 
+// Components
+import Separator from './Separator'
+
 const Skills = () => {
 
+   const skillsData = [
+      {
+         type: 'Frontend',
+         list: [
+            {
+               name: 'React.js',
+               icon: <i className="devicon-react-original colored"></i>
+            },
+            {
+               name: 'JavaScript',
+               icon: <i className="devicon-javascript-plain colored"></i>
+            },
+            {
+               name: 'Redux',
+               icon: <i className="devicon-redux-original colored"></i>
+            },
+            {
+               name: 'Jest',
+               icon: <i className="devicon-jest-plain colored"></i>
+            },
+            {
+               name: 'CSS',
+               icon: <i className="devicon-css3-plain colored"></i>
+            }
+         ]
+      },
+      {
+         type: 'Backend',
+         list: [
+            {
+               name: 'Node.js',
+               icon: <i className="devicon-nodejs-plain colored"></i> 
+            },
+            {
+               name: 'Express',
+               icon: <i className="devicon-express-original colored"></i>
+            }
+         ]
+      }
+   ]
+
    return(
-      <div>
-         Skills component
+      <div className="skills-container">
+         <Separator />
+         <label className="section-title">Skills</label>
+            <div className='skills-list-container'>
+               {skillsData.map((item) => {
+                  return (
+                     <div className='skills-section' key={item.type}>
+                        <label className='skills-section-title'>{item.type}</label> 
+                           <div className='skills-list'>
+                              {item.list.map((skill) => {
+                                 return(
+                                    <div className="skill-card" key={skill.name}> 
+                                       <div className="skill-icon">{skill.icon}</div>
+                                       <label className='skill-name'>{skill.name}</label>
+                                    </div>
+                                 )})}
+                           </div>
+                     </div>
+                  )})}
+            </div>
       </div>
    )
 }
