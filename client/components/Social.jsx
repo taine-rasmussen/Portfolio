ec19463066346dcd73db0b6c901f7d36b6a8cc89
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-17 14:46:50 +1300

    created object of all data for social links

---
+++ client/components/Social.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Social = () => {
+
+   const socialData = [
+      {
+         platform: 'Instagram',
+         icon: 'public/instagram.png',
+         link: 'https://www.instagram.com/tainerasmussen/'
+      },
+      {
+         platform: 'Github',
+         icon: 'public/github-sign.png',
+         link: 'https://github.com/taine-rasmussen'
+      },
+      {
+         platform: 'Linkedin',
+         icon: 'public/linkedin.png',
+         link: 'https://www.linkedin.com/in/taine-rasmussen-a66a86184/'
+      },
+   ]
+
+   return(
+      <div>
+
+      </div>
+   )
+}
+
+export default Social
