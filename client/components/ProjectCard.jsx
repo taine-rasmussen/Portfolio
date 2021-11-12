fe717aba54165e6a39e3d955790026f069d47cd8
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-12 16:17:12 +1300

    added images and tags

--- client/components/ProjectCard.jsx
+++ client/components/ProjectCard.jsx
@@ -8,11 +8,30 @@ const ProjectCard = ({ project }) => {
 
    return(
       <div className="project-card-container">
+
          <div className="project-card-title">
             <h2>{project.title}</h2>
          </div>
-         <div className="project-card-info">
+         
+         <div className="project-card-info-container">
+            <div className="project-card-info">
+               <p>{project.about}</p>
+            </div>
+            <div className="project-card-info">
+               <img src={project.image} alt={project.title}/>
+            </div>
+         </div>
 
+         <div className="project-card-footer-container">
+            <div className="footer-btns">
+               {project.tags.map((item, index) => {
+                  return(
+                     <div className="project-card-tag" key={index}>
+                        {item}
+                     </div>
+                  )
+               })}
+            </div>
          </div>
       </div>
    )
